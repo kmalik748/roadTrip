ed89135f3f2d136c7d8830ed50ab0953b2e64ae6
kmali <kmalik748@gmail.com>
2021-06-04 12:25:38 +0500

    new`

--- modules/db.php
+++ modules/db.php
@@ -4,10 +4,10 @@ $username = "bxbnuxqkra";
 $password = "4wzFy9nVKJ";
 $dbname = "bxbnuxqkra";
 
-//$servername = "localhost";
-//$username = "root";
-//$password = "";
-//$dbname = "project_roadtrip";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project_roadtrip";
 
 
 $con = mysqli_connect($servername, $username, $password, $dbname);
