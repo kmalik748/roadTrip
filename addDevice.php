43e9abd5c98b5a1aa4527d3bdfda01705f9bc127
kmali <kmalik748@gmail.com>
2021-06-06 15:30:01 +0500

    new`

--- addDevice.php
+++ addDevice.php
@@ -3,6 +3,8 @@ require 'modules/apiCore.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
+
+$uniqueID = isset($data->deviceUniqueID) ? filterData($data->deviceUniqueID) : "";
 $lang = isset($data->lang) ? filterData($data->lang) : "";
 $type = isset($data->type) ? filterData($data->type) : "";
 $os_version = isset($data->os_version) ? filterData($data->os_version) : "";
@@ -11,17 +13,38 @@ $app_version = isset($data->app_version) ? filterData($data->app_version) : "";
 $model = isset($data->model) ? filterData($data->model) : "";
 $firebaseKey = isset($data->firebaseKey) ? filterData($data->firebaseKey) : "";
 
-$sql = "INSERT INTO devices_info (lang, type, os_version, carrier, app_version, model, app_member_id, firebase_key) VALUES 
-            ('$lang', '$type', '$os_version', '$carrier', '$app_version', '$model', '', '$firebaseKey')";
-if(mysqli_query($con, $sql)){
-    $currentUserId = mysqli_insert_id($con);
-    $response["Error"] = false;
-    $response["Info"]["msg"] = "New device added successfully";
+$query = mysqli_query($con, "SELECT * FROM devices_info WHERE app_member_id = '$uniqueID'");
+if(mysqli_num_rows($query)){
+    $sql = "UPDATE devices_info SET lang='$lang', type='$type', os_version='$os_version', carrier='$carrier', app_version='$app_version',
+            model='$model', firebase_key='$firebaseKey' WHERE app_member_id = '$uniqueID'";
+    if(mysqli_query($con, $sql)){
+        $currentUserId = mysqli_insert_id($con);
+        $response["Error"] = false;
+        $response["Info"]["msg"] = "Device Details updated successfully";
+    }else{
+        $response["Error"] = true;
+        $response["Info"]["msg"] = mysqli_error($con);
+        echo json_encode($response); exit(); die();
+    }
+    $s = "SELECT * FROM devices_info WHERE app_member_id = '$uniqueID'";
+    $r = mysqli_query($con, $s);
+    $ro = mysqli_fetch_array($r);
+    $response["deviceID"] = $ro["id"];
 }else{
-    $response["Error"] = true;
-    $response["Info"]["msg"] = mysqli_error($con);
-    echo json_encode($response); exit(); die();
+    $sql = "INSERT INTO devices_info (lang, type, os_version, carrier, app_version, model, app_member_id, firebase_key) VALUES 
+            ('$lang', '$type', '$os_version', '$carrier', '$app_version', '$model', '$uniqueID', '$firebaseKey')";
+    if(mysqli_query($con, $sql)){
+        $currentUserId = mysqli_insert_id($con);
+        $response["Error"] = false;
+        $response["Info"]["msg"] = "New device added successfully";
+    }else{
+        $response["Error"] = true;
+        $response["Info"]["msg"] = mysqli_error($con);
+        echo json_encode($response); exit(); die();
+    }
+    $response["deviceID"] = $currentUserId;
 }
-$response["deviceID"] = $currentUserId;
+
+
 
 echo json_encode($response);
--- getGeoBaseData.php
+++ getGeoBaseData.php
@@ -9,6 +9,7 @@ $long = isset($data->long) ? filterData($data->long) : "";
 $radius = (int) isset($data->radius) ? filterData($data->radius) : "";
 $firebaseKey = isset($data->firebaseKey) ? filterData($data->firebaseKey) : "";
 $categories = isset($data->categories ) ? filterData($data->categories ) : "";
+$development = isset($data->developmntPhase ) ? filterData($data->developmntPhase ) : "";
 $currentUserId = null;
 
 $sql = "SELECT * FROM devices_info WHERE id =$uniqueID LIMIT 1";
@@ -27,53 +28,45 @@ $response["userID"] = $currentUserId;
 $sql = "UPDATE devices_info SET firebase_key='$firebaseKey' WHERE id=$currentUserId";
 mysqli_query($con, $sql);
 
-// check for the nearest locations
-
-$sql = "SELECT id, title, latitude, longitude, marker_radius, amazon_polly_audio_link_location as audio, opening_sound, closing_sound, categories, country, image, region, story_type, eventdate,
+if($development=="yes"){
+    $response["geoLocations"] = array();
+    $sql = "SELECT id, title, latitude, longitude, marker_radius, amazon_polly_audio_link_location as audio, opening_sound, closing_sound,
+       categories, country, image, region, story_type, eventdate
+        FROM wp_audio_info";
+    $res = mysqli_query($con, $sql);
+    $geoLocations = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    foreach ($geoLocations as $location){
+        array_push($response["geoLocations"], $location);
+    }
+    echo json_encode($response); exit(); die();
+}else{
+    // check for the nearest locations
+    $sql = "SELECT id, title, latitude, longitude, marker_radius, amazon_polly_audio_link_location as audio, opening_sound, closing_sound, categories, country, image, region, story_type, eventdate,
        ( 3959 * acos( cos( radians($lat) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians($long) ) + sin( radians($lat) ) * sin(radians(latitude)) ) ) AS distance
         FROM wp_audio_info WHERE categories = '$categories' HAVING distance < $radius ORDER BY distance LIMIT 0 , 20";
-//echo $sql;
-$res = mysqli_query($con, $sql);
-$newLocations = array();
-if(mysqli_num_rows($res)){
-     $geoLocations = mysqli_fetch_all($res, MYSQLI_ASSOC);
-     mysqli_free_result($result);
-     require 'modules/timezone.php';
-     $dateNow = date("Y-m-d");
-     $s = "SELECT * FROM sent_requests WHERE Date(date_now) = '$dateNow' AND device_id=$currentUserId";
-     $r = mysqli_query($con, $s);
-     $sentRequests = mysqli_fetch_all($r, MYSQLI_ASSOC);
-
-     $sentArray = array();
-     foreach ($sentRequests as $rqst){
-         if(isset($rqst["id"])){
-             array_push($sentArray, $rqst["location_id"]);
-         }
-     }
-//     echo "SENT LOCATIONS: ";
-//     print_r($sentArray);
-     foreach ($geoLocations as $location){
-
-//         echo "nwe locations: ".$location["id"];
-        if(!in_array($location["id"], $sentArray)){
-            array_push($newLocations, $location);
-            locationWasSent($currentUserId, $location["id"]);
+    $res = mysqli_query($con, $sql);
+    if(mysqli_num_rows($res)){
+        $geoLocations = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        mysqli_free_result($result);
+        require 'modules/timezone.php';
+        $dateNow = date("Y-m-d");
+        $s = "SELECT * FROM sent_requests WHERE Date(date_now) = '$dateNow' AND device_id=$currentUserId";
+        $r = mysqli_query($con, $s);
+        $sentRequests = mysqli_fetch_all($r, MYSQLI_ASSOC);
+        $sentArray = array();
+        foreach ($sentRequests as $rqst){
+            if(isset($rqst["id"])){
+                array_push($sentArray, $rqst["location_id"]);
+            }
         }
-     }
-
-
-//    foreach ($geoLocations as $location){
-//        print_r($sentRequests);
-//     foreach ($sentRequests as $sentLocation){
-//            echo $location["id"].'     ---      '.$sentLocation["location_id"].'<br>';
-////            if($location["id"]!=$sentLocation["id"]){
-////                array_push($newLocations, $location);
-////                locationWasSent($currentUserId, $location["id"]);
-////            }
-//        }
-//     }
-     $response["geoLocations"] = $newLocations;
-
+        foreach ($geoLocations as $location){
+            if(!in_array($location["id"], $sentArray)){
+                array_push($newLocations, $location);
+                locationWasSent($currentUserId, $location["id"]);
+            }
+        }
+        $response["geoLocations"] = $newLocations;
+    }
 }
 
 
